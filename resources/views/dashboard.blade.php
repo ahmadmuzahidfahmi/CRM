f2d1a5588fc6e2e48db0c014bd74e457e55049b6
ahmadmuzahidfahmi <ahmadmuzahid557@gmail.com>
2026-06-25 16:50:16 +0800

    Test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
     </button>
     <span class="ml-3 text-lg font-bold" x-text="count"></span>
 
-    <div x-data="{ message: 'meow!' }">
+    <div x-data="{ message: 'test!' }">
     <p x-text="message"></p>
 </div>
 
